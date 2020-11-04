062b695c8981c4a7930423b884629747a7109114
prakashupes <85prakash2017@gmail.com>
2020-11-04 13:46:25 +0530

    Build

---
+++ build/CMakeFiles/MyExec.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/prakash/DBPlus-Relational-Database-With-Minimal-Functionality/src/main.cpp" "/home/prakash/DBPlus-Relational-Database-With-Minimal-Functionality/build/CMakeFiles/MyExec.dir/src/main.cpp.o"
+  "/home/prakash/DBPlus-Relational-Database-With-Minimal-Functionality/src/queryProcess/create.cpp" "/home/prakash/DBPlus-Relational-Database-With-Minimal-Functionality/build/CMakeFiles/MyExec.dir/src/queryProcess/create.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MyExec.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExec.dir/src/main.cpp.o"
+  "CMakeFiles/MyExec.dir/src/queryProcess/create.cpp.o"
+  "MyExec"
+  "MyExec.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExec.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
