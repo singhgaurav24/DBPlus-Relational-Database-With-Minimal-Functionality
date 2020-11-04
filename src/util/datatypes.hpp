4dec9d358d4ec6f9d06a5bef906763dfd301d31b
prakashupes <85prakash2017@gmail.com>
2020-11-04 16:15:09 +0530

    [src] data types class

---
+++ src/util/datatypes.hpp
@@ -0,0 +1,35 @@
+#include<set>
+#include<string>
+#ifndef DATATYPES_HPP
+#define DATATYPES_HPP
+using namespace std;
+class DATATYPES
+{
+    private:
+    set<string> data_types;
+   
+    
+
+    public:
+    DATATYPES()
+    {
+        data_types.insert("VARCHAR");
+        data_types.insert("INT");
+        data_types.insert("INTEGER");
+        data_types.insert("TEXT");
+        data_types.insert("DECIMAL");
+        data_types.insert("DOUBLE");
+        data_types.insert("LONG");
+
+    }
+    bool exist_DataTypes(string t)
+    {
+        if(data_types.find(t)!=data_types.end()) return true;
+
+        return false;
+        
+    }
+};
+
+
+#endif
