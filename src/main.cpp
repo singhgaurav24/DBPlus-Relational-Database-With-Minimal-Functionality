d7a36164cf18821b1622dfe4506e1765c02c0a76
prakashupes <85prakash2017@gmail.com>
2020-11-05 23:44:06 +0530

    [src] raw changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 #include <string> 
 #include "util/util.hpp"
+#include "util/enums.hpp"
 #include "include/create.hpp"
 #include "include/executeQuery.hpp"
+#include "include/validateQuery.hpp"
+#include "/usr/local/include/hsql/SQLParser.h"
+
 using namespace std;
 
 
 
 int main() {
     string query;
-    
-    char *err;
+    string err_msg;
+    QType qType;
     cout<<"Runnig databse DBplus>>\nTo exit from database type q or quit \nFor any kind of help type --help \n\n";
     while (1)
     {
@@ -22,15 +26,24 @@ int main() {
         /**TODO**
          * Validte query
          * Execute query
+         * Make data type as enum
+         * 
         */
-       ExecuteQuery executeQuery;
-       if(executeQuery.isValid(query,err)) cout<<"Yes";
+       //ValidateQuery executeQuery;
+       //if(!executeQuery.isValid(query,err_msg,qType)) cout<<"\n"<<query<<" "<<err_msg<<endl;
+
+       //else cout<<"valid"<<endl;
+         hsql::SQLParserResult result;
+         hsql::SQLParser::parse(query, &result);
+         if (!result.isValid()) 
+         {
+             cout<<"Invalid\n";
+         }
+         else cout<<"Valid query\n";
 
 
 
-
-
-
+    }
     return 0;
 
 }
