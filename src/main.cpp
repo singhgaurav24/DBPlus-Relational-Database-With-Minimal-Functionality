08f536fdd0953f3d94411c72615ab16480798e89
prakashupes <85prakash2017@gmail.com>
2020-11-05 14:11:40 +0530

    [src] code moved

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,16 @@
 #include<iostream>
 #include <string> 
-#include<vector>
-#include<map>
 #include "util/util.hpp"
 #include "include/create.hpp"
+#include "include/executeQuery.hpp"
 using namespace std;
 
-int tokenHash(string su)
-{
-    map<string,int> hash;
-    toUpper(su);
-    hash.insert({"CREATE",101});
-    hash.insert({"INSERT",102});
-    hash.insert({"DELETE",103});
-    hash.insert({"SELECT",104});
-    auto it=hash.find(su);
-    return it->second;
 
-}
 
 int main() {
     string query;
-    vector<string> token;
+    
+    char *err;
     cout<<"Runnig databse DBplus>>\nTo exit from database type q or quit \nFor any kind of help type --help \n\n";
     while (1)
     {
@@ -30,31 +19,17 @@ int main() {
         if(query=="q" || query=="quit") exit(1);
         if(query.size()<1) continue;
 
-        
-        token=split(query,' ');
-        int hash=tokenHash(token[0]);
-        isCreatevalidate(query,token);
-        //cout<<hash;
-        switch (hash)
-        {
-        case 101:
-           
-            break;
-        case 102:
-            cout<<"i";
-            break;
-        case 103:
-            cout<<"d";
-            break;
-        case 104:
-            cout<<"s";
-            break;
-        
-        default:
-            cout<<"INVALID QUERY"<<endl;
-            break;
-        }        
-    }
+        /**TODO**
+         * Validte query
+         * Execute query
+        */
+       ExecuteQuery executeQuery;
+       if(executeQuery.isValid(query,err)) cout<<"Yes";
+
+
+
+
+
 
     return 0;
 
