9ee5e5216d38623d8ffcce4c9a99da6d13bec380
prakashupes <85prakash2017@gmail.com>
2020-12-27 01:14:18 +0530

    [src] new update template

--- src/queryProcess/select.cpp
+++ src/queryProcess/select.cpp
@@ -1,36 +1,35 @@
 #include<iostream>
 #include<vector>
 #include<string>
-#include "../include/create.hpp"
-#include"../util/datatypes.hpp"
+#include "../include/select.hpp"
 #include"../util/util.hpp"
+#include "/usr/local/include/hsql//sql/statements.h"
 using namespace std;
-bool isCreatevalid(vector<string> token, string &err)
-{
-    //cout<<"From cpp "<<a;
-    if(token.size()<4) 
-    {
-        err="Possible SYNTAX Error at CREATE ^ table....Or incomplete query";
-        return false;
-    }
-    toUpper(token[1]);
-    if(token[1]!= "TABLE")
-    {
-        err="Possible SYNTAX Error at CREATE ^ table....Or incomplete query";
-        return false;
-    } 
-    string table=token[2];
-    if(!token[3][0]=='(')
-    {
-        //Check balnced paranthesis if()
-        err= "Possible SYNTAX Error at (....Or incomplete query";
-        return false;
-    }
-    //Logic for balanced paranthesis
-    DataType dtype;
-    if(!dtype.exist_DataTypes("VARCHAR"))
-    {
+namespace dbplus{
+    
+        void Select::selectStatement(const hsql::CreateStatement* create_stmt)
+        {
+            string str="";
+            cout<<"create statement\n"<<endl;
+     //const CreateStatement* create_stmt = (const CreateStatement*) stmt; //typecast
+     cout<<"fields "<<endl;
+     cout<<create_stmt->tableName<<endl;
+    // std::vector<char*>* viewColumns = create_stmt->viewColumns;
+     //cout<<viewColumns->size()<<endl;
+     //cout<<viewColumns->at(0);  
+     if (create_stmt->columns != nullptr) {
+      
+      for (auto col_name : *create_stmt->columns) {
         
+          cout<<col_name->name<<endl;
+      }
     }
-    return true;
+    
+        }
+        
+    
+    
+
+
+
 }
