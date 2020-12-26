dfdd2c6fda8fcedcca179be71c94100f395afa2d
prakashupes <85prakash2017@gmail.com>
2020-12-26 14:31:49 +0530

    [src] new flow in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,24 @@
 #include<iostream>
-#include <string> 
+#include <string>
+#include<vector>
 #include "util/util.hpp"
 #include "util/enums.hpp"
 #include "include/create.hpp"
 #include "include/executeQuery.hpp"
 #include "include/validateQuery.hpp"
-#include "/usr/local/include/hsql/SQLParser.h"
+#include "/usr/local/include/hsql/util/sqlhelper.h"
+
+
+using namespace hsql;
 
 using namespace std;
 
-
+//class Driver
 
 int main() {
     string query;
     string err_msg;
-    QType qType;
+    
     cout<<"Runnig databse DBplus>>\nTo exit from database type q or quit \nFor any kind of help type --help \n\n";
     while (1)
     {
@@ -29,18 +33,55 @@ int main() {
          * Make data type as enum
          * 
         */
+
+
+       vector<const SQLStatement* > stmts;
        dbplus::ValidateQuery validateQuery;
-       if(!validateQuery.isValid(query,err_msg,qType)) 
+       if(!validateQuery.isValid(query,err_msg,stmts)) 
        {
-           //cout<<"\n"<<query<<" "<<err_msg<<endl;
+           cout<<err_msg<<endl;
+           return 0;
        }
 
-       else cout<<"valid"<<endl;
-    
+       cout<<stmts.size()<<endl;
 
+         
+     /*
+     s
+     switch (stmt->type())
+     {
+     case kStmtCreate:
+     printInfo((const CreateStatement*) stmt);
+      break;
+     case kStmtSelect:
+      //cout<<"Tbale name " <<((const SelectStatement *) stmt)->tableName;
+      break;
+    case kStmtInsert:
+      printInsertStatementInfo((const InsertStatement*) stmt, 0);
+      break;
     
+    case kStmtUpdate:
+     // cout<<"Tbale name" <<((const UpdateStatement *) stmt)->tableName;
+      break;
+    case kStmtExport:
+      printExportStatementInfo((const ExportStatement*) stmt, 0);
+      break;
+    case kStmtTransaction:
+      printTransactionStatementInfo((const TransactionStatement*) stmt, 0);
+      break;
+    default:
+      break;
+     }
 
     }
+
+      // dbplus::ValidateQuery::isValid(query,err_msg,qType);
+       //else cout<<"valid"<<endl;
+    
+
+    */
+
+}
     return 0;
 
 }
