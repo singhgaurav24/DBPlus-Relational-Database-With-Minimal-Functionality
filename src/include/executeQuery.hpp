73139322a92a48ff4c3899802b86e8d5b80e80b9
prakashupes <85prakash2017@gmail.com>
2020-12-28 12:25:23 +0530

    execute new format

--- src/include/executeQuery.hpp
+++ src/include/executeQuery.hpp
@@ -1,6 +1,7 @@
 #include <string>
 #include<vector>
 #include "/usr/local/include/hsql/sql/statements.h"
+#include "/usr/local/include/hsql/SQLParser.h"
 #ifndef EXECUTE_QUERY_HPP
 #define EXECUTE_QUERY_HPP
 
@@ -16,7 +17,7 @@ private:
 public:
     //executeQuery();
     //~executeQuery();
-    void executeQuery(std::vector<const hsql::SQLStatement* > &stmts);
+    void executeQuery(hsql::SQLParserResult &result);
     
 
 };
--- src/queryProcess/executeQuery.cpp
+++ src/queryProcess/executeQuery.cpp
@@ -1,25 +1,39 @@
 #include <string>
 #include "../include/executeQuery.hpp"
-
+#include "../include/global.hpp"
+#include "../include/create.hpp"
+#include <iostream>
 using namespace std;
 using namespace hsql;
+using namespace dbplus;
 namespace dbplus{
 
-void ExecuteQuery::executeQuery(std::vector<const hsql::SQLStatement* > &stmts)
+void ExecuteQuery::executeQuery(hsql::SQLParserResult &result)
 {
 
-    //we will again call validate function to check if query exist or not,
+
+//First it preprocess the data
+
+
+      //we will again call validate function to check if query exist or not,
     //cout<<"Tbale name " <<((const SelectStatement *) stmt)->tableName;
-    for(auto stmt: stmts) //x is sql statement
-    {
+
+    PreprocessData data;
+    data.preprocess();
+
+    for (auto i = 0u; i < result.size(); ++i) {
+     
+
+        const SQLStatement* stmt = result.getStatement(i);
         switch (stmt->type())
         {
             case kStmtCreate:
-
+                Create create;
+                create.createStatement((const CreateStatement*)stmt);
             
             break;
             case kStmtSelect:
-      
+                cout<<"Select ";
             break;
             case kStmtInsert:
      
@@ -36,6 +50,39 @@ void ExecuteQuery::executeQuery(std::vector<const hsql::SQLStatement* > &stmts)
             break;
      }
     }
+
+
+    /*
+    for(const hsql::SQLStatement*stmt: stmts) //x is sql statement
+    {
+        switch (stmt->type())
+        {
+            case kStmtCreate:
+                cout<<"Create ";
+                Create create;
+                cout<<((const CreateStatement*)stmt)->tableName<<endl;
+                create.createStatement((const CreateStatement*)stmt);
+            
+            break;
+            case kStmtSelect:
+                cout<<"Select ";
+            break;
+            case kStmtInsert:
+     
+            break;
+    
+            case kStmtUpdate:
+     // cout<<"Tbale name" <<((const UpdateStatement *) stmt)->tableName;
+            break;
+            case kStmtDelete:
+      
+            break;
+   
+            default:
+            break;
+     }
+    }
+    */
 }
 
 }
